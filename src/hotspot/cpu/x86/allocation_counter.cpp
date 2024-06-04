50a7a75d8c84904d4d8b8a029da468546c2a10d8
Xue Haonan <xuehaonan27@gmail.com>
2024-06-04 21:07:18 +0800

    feat: interpreter.

--- src/hotspot/cpu/x86/allocation_counter.cpp
+++ src/hotspot/cpu/x86/allocation_counter.cpp
@@ -2,15 +2,15 @@
 #define XHN_JVM_X86_ALLOCATION_COUNTER_HPP
 
 #include "allocation_counter.hpp"
-AtomicSizet RuntimeAllocationCounter::interpreter_fast_tlab_cnt = AtomicSizet2();
-AtomicJLong RuntimeAllocationCounter::interpreter_fast_tlab_time = AtomicJLong2();
+AtomicSizet2 RuntimeAllocationCounter::interpreter_fast_tlab_cnt = AtomicSizet2();
+AtomicJLong2 RuntimeAllocationCounter::interpreter_fast_tlab_time = AtomicJLong2();
 size_t RuntimeAllocationCounter::interpreter_fast_tlab_cnt_raw = 0;
 size_t RuntimeAllocationCounter::interpreter_fast_eden_cnt_raw = 0;
 size_t RuntimeAllocationCounter::interpreter_slow_cnt_raw = 0;
 
-AtomicSizet RuntimeAllocationCounter::interpreter_fast_eden_cnt = AtomicSizet2();
-AtomicJLong RuntimeAllocationCounter::interpreter_fast_eden_time = AtomicJLong2();
+AtomicSizet2 RuntimeAllocationCounter::interpreter_fast_eden_cnt = AtomicSizet2();
+AtomicJLong2 RuntimeAllocationCounter::interpreter_fast_eden_time = AtomicJLong2();
 
-AtomicSizet RuntimeAllocationCounter::interpreter_slow_cnt = AtomicSizet2();
-AtomicJLong RuntimeAllocationCounter::interpreter_slow_time = AtomicJLong2();
+AtomicSizet2 RuntimeAllocationCounter::interpreter_slow_cnt = AtomicSizet2();
+AtomicJLong2 RuntimeAllocationCounter::interpreter_slow_time = AtomicJLong2();
 #endif
